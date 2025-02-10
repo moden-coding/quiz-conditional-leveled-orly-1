a57d1fae9e00f344a3cd38d1852ece14bf693fed
Orly <25obolan@gannacademy.org>
2025-02-10 13:27:28 -0500

    done

--- src/App.java
+++ src/App.java
@@ -23,8 +23,32 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
 
+        if (number%2 == 0) {
+            System.out.println("The number is even.");
+        } if (number%2 != 0) {
+            System.out.println("The number is odd.");
+        }
 
+        if (number%3 == 0 && number%5 != 0) {
+            System.out.println("The number is divisible by 3.");
+        }  if (number%5 == 0 && number%3 != 0) {
+            System.out.println("The number is divisible by 5.");
+        }  else if (number%3 == 0 && number%5 == 0) {
+            System.out.println("The number is divisible by both 3 and 5.");
+        }
+
+        if (number < 0 || number > 100) {
+            System.out.println("Wrong number!");
+        } 
+        
+        if (number == 7 || number == 5 || number == 3 || number == 2) {
+            System.out.println("The number is prime.");
+        } else if (number%number == 0 && number%1 == 0 && number%2 != 0 && number%3 != 0 && number%5 != 0 && number%7 != 0) {
+            System.out.println("The number is prime.");
+        } 
         
     }
 }
